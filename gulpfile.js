b872ac789cbc2b130e678b8ef0ab5419d8a8f767
ManGo-WEB Studio <info@mango-web.ru>
2017-05-27 09:05:17 +0800

    add var=path gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,35 @@ var gulp = require('gulp'),
 		rimraf = require('rimraf'),
 		browserSync = require('browser-sync'),
 		reload = browserSync.reload;
+
+var path = {
+	build: { //Куда складывать готовые файлы после сборки
+		html: 'build',
+		js: 'build/js',
+		css: 'build/css',
+		img: 'build/img',
+		fonts: 'build/fonts'
+	},
+	src: { //Откуда брать исходники
+		html: 'src/*.html', //Хотим взять все файлы с расширением .html
+		js: 'src/js/main.js',
+		style: 'src/scss/main.scss',
+		img: 'src/img/**/*.*', //Взять все файлы, всех расширений из всех вложенных папок
+		fonts: 'src/fonts/**/*.*' 
+	},
+	watch: { //Наблюдаем за изменениями файлов
+		html: 'src/**/*.html',
+		js: 'src/js/**/*.js',
+		style: 'src/scss/**/*.scss',
+		img: 'src/img/**/*.*',
+		fonts: 'src/fonts/**/*.*'
+	},
+	clean: './build'
+}
+
+
+
+
+
+
+
