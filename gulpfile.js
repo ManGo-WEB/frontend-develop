d4e970f6260a818eaa08e0a47fab8cebdd04b811
ManGo-WEB Studio <info@mango-web.ru>
2017-05-27 11:23:06 +0800

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -108,6 +108,25 @@ gulp.task('build', [
 	'image:build'
 ]);
 
+// Watcher
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
 
 
 
